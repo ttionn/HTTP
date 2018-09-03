2dd7ab764e2f2ad859c6db46527f087ff7bad1e6
ttionn <ttionn@icloud.com>
2018-09-03 14:35:55 +0800

    Add Tests

---
+++ HTTP/Source/HTTP.swift
@@ -0,0 +1,13 @@
+//
+//  HTTP.swift
+//  HTTP
+//
+//  Created by TTSY on 2018/9/3.
+//  Copyright © 2018 TTSY. All rights reserved.
+//
+
+import Foundation
+
+public struct HTTP {
+    
+}
---
+++ HTTPTests/HTTPTests.swift
@@ -0,0 +1,35 @@
+//
+//  HTTPTests.swift
+//  HTTPTests
+//
+//  Created by TTSY on 2018/9/3.
+//  Copyright © 2018 TTSY. All rights reserved.
+//
+
+import XCTest
+
+class HTTPTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
