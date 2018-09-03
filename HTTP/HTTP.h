036268549692085e8968cc5db8a1dfe5ccd697d7
ttionn <ttionn@icloud.com>
2018-09-03 14:22:22 +0800

    Initial Commit

---
+++ HTTP/HTTP.h
@@ -0,0 +1,19 @@
+//
+//  HTTP.h
+//  HTTP
+//
+//  Created by TTSY on 2018/8/22.
+//  Copyright © 2018 TTSY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HTTP.
+FOUNDATION_EXPORT double HTTPVersionNumber;
+
+//! Project version string for HTTP.
+FOUNDATION_EXPORT const unsigned char HTTPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTTP/PublicHeader.h>
+
+
